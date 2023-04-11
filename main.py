9eb75f3bda6590e159d64759425243629fcaf960
Sept <sszheng3@gmail.com>
2023-04-10 20:30:44 -0400

    top 10

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     ratings, recipe_ids = recipe_recommendor.__createrecommendations__(author_id=test_userId,
                                                                        recipe_ids=get_similar_recipes(test_userId, data.clustering_df, category="Dessert"))
 
-    top_recipe_ids = [recipe_ids[i] for i in sorted(range(len(ratings)), key=lambda i: ratings[i], reverse=True)[:10]] if len(ratings) > 0 else recipe_ids[:10]
+    top_recipe_ids = [recipe_ids[i] for i in sorted(range(len(ratings)), key=lambda i: ratings[i], reverse=True)[:10]]
     print("top recipe ids for user: "+str(test_userId))
     print(top_recipe_ids)
 
